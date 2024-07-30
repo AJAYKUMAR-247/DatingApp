286871b9290e8e5303f675acfd16481da1179ac2
AJAYKUMAR V <ajaykumarak1829@gmail.com>
2024-07-30 22:50:02 +0530

    section 2

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API;
+namespace API.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController(DataContext context) : ControllerBase
